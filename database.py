4bdbe4ae4de5a5b5f2b1788f74a5e219198440a2
fastfingertips <fastfingertips@gmail.com>
2023-10-11 23:57:49 +0300

    Updated db_name

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from application import app
 import os
 
 app_path = os.path.dirname(os.path.abspath(__file__))
-db_name = "todo.db"
+db_name = "articles.db"
 db_path = os.path.join(app_path, db_name)
 sqlite_path = 'sqlite:///' + db_path
 app.config['SQLALCHEMY_DATABASE_URI'] = sqlite_path
