75e7ead84b877a8d0e054b156f9948d2f9325bf9
fastfingertips <fastfingertips@gmail.com>
2023-10-14 23:55:48 +0300

    Added: new lib and functions
    get_story_from_db,
    get_stories

--- functions.py
+++ functions.py
@@ -1,6 +1,7 @@
 from database import db
 import requests, time
 from datetime import datetime
+from tqdm import tqdm
 
 def getCurrentTime():
     return datetime.now()
@@ -52,6 +53,8 @@ def articleParser(article_json):
             else: print(f'{getKey} not found')
     return data
 
+# -- URL --
+
 def delete_db_blank_urls():
     from models import Url
     articles = Url.query.filter_by(url='').all()
@@ -68,3 +71,95 @@ def get_db_counts():
         'favorites': Url.query.filter_by(favorite=True).count(),
     }
     return counts
+
+def get_story_from_db(article_id):
+    from models import Url
+    article = Url.query.filter_by(article_id=article_id).first()
+    if article is None:
+        return None
+    else:
+        return article
+
+def get_stories(article_limit=None, score_limit=None):
+    from models import Url
+    articleIds = getTopStories() + getBestStories() + getNewStories()
+    print(len(articleIds), 'article ids found')
+    articleIds = list(set(articleIds))
+    print(len(articleIds), 'unique article ids found')
+    articles = []
+    already_visited_count = 0
+    already_in_db_count = 0
+    added_to_db_count = 0
+    no_url_count = 0
+
+    for current_article_id in tqdm(articleIds, desc='Getting stories'):
+        # get article from database
+        article = get_story_from_db(current_article_id)
+
+        # check if article is already in database
+        if article is None:
+
+            # get article from api
+            articleJson = getArticle(current_article_id)
+            articleDict = articleParser(articleJson)
+
+            # if url is not valid, skip article
+            if articleDict['url'] == '':
+                """
+                Already in database, but url is empty
+                DELETE FROM url WHERE url = ''
+                SELECT * FROM url WHERE url = ''
+                """
+                # print(rank, current_article_id, 'no url, skipping')
+                no_url_count += 1
+                continue
+
+            new_article = Url(
+                article_id = articleDict['id'],
+                by=articleDict['by'],
+                score=articleDict['score'],
+                time=articleDict['time'],
+                title=articleDict['title'],
+                url=articleDict['url'],
+            )
+
+            db.session.add(new_article)
+            db.session.commit()
+
+            # print(rank, current_article_id, 'added to database')
+            added_to_db_count += 1
+            
+            # get article from database
+            article = get_story_from_db(current_article_id)
+        else:
+            # print(rank, current_article_id, 'already in database')
+            already_in_db_count += 1
+            if article.visited:
+                # print(rank, current_article_id, 'already visited')
+                already_visited_count += 1
+                continue
+
+        if score_limit is not None:
+            if article.score < score_limit:
+                # print(current_article_id, 'score too low, skipping')
+                continue
+
+        articles.append(article)
+
+        if article_limit is not None:
+            if len(articles) >= article_limit:
+                # if article limit reached, stop adding articles
+                break
+
+    data = {
+        'already_in_db_count': already_in_db_count,
+        'already_visited_count': already_visited_count,
+        'not_visited_count': already_in_db_count - already_visited_count,
+        'added_to_db_count': added_to_db_count,
+        'no_url_count': no_url_count,
+    }
+
+    for k, v in data.items():
+        print(k.ljust(25), v)
+
+    return articles
--- routes.py
+++ routes.py
@@ -12,84 +12,20 @@ from functions import (
     get_db_counts,
     getArticle,
     articleParser,
-    getCurrentTime
+    getCurrentTime,
+    get_story_from_db,
+    get_stories
 )
 
 from application import app
 from database import db
 from models import Url
 
+
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    article_limit = 30
-    score_limit = 0
-    articleIds = getTopStories() + getBestStories() # + getNewStories()
-    print(len(articleIds), 'article ids found')
-    articleIds = list(set(articleIds))
-    print(len(articleIds), 'unique article ids found')
-    articles = []
-
-    visited_count = Url.query.filter_by(visited=True).count()
-    print(f'visited count: {visited_count}')
-
-    for rank, current_article_id in enumerate(articleIds):
-        # get article from database
-        article = Url.query.filter_by(
-            article_id=current_article_id
-            ).first()
-
-        # check if article is already in database
-        if article is None:
-
-            # get article from api
-            articleJson = getArticle(current_article_id)
-            articleDict = articleParser(articleJson)
-
-            # if url is not valid, skip article
-            if articleDict['url'] == '':
-                """
-                Already in database, but url is empty
-                DELETE FROM url WHERE url = ''
-                SELECT * FROM url WHERE url = ''
-                """
-                print(rank, current_article_id, 'no url, skipping')
-                continue
-
-            new_article = Url(
-                article_id = articleDict['id'],
-                by=articleDict['by'],
-                score=articleDict['score'],
-                time=articleDict['time'],
-                title=articleDict['title'],
-                url=articleDict['url'],
-            )
-
-            db.session.add(new_article)
-            db.session.commit()
-
-            print(rank, current_article_id, 'added to database')
-            
-            # get article from database
-            article = Url.query.filter_by(
-                article_id=current_article_id
-                ).first()
-        else:
-            print(rank, current_article_id, 'already in database')
-            if article.visited:
-                print(rank, current_article_id, 'already visited')
-                continue
-
-        """
-        if article.score < score_limit:
-            print(rank, current_article_id, 'score too low, skipping')
-            continue
-        """
-
-        articles.append(article)
-                
-        if len(articles) >= article_limit:
-            # if article limit reached, stop adding articles
-            break
+    articles = get_stories(30)
 
     articles_sorted = sorted(articles, key=lambda x: x.score, reverse=True)
     context = {
@@ -97,8 +33,6 @@ def index():
         'articles': articles_sorted,
         'theme': 'dark',
     }
-
-    print(len(articles), 'articles added to context')
     
     return render_template('index.html', **context)
 
@@ -119,10 +53,11 @@ def visited():
 def notvisited():
     articles = Url.query.filter_by(visited=False).all()
     articles = sorted(articles, key=lambda x: x.score, reverse=True)
+    articles_top = articles[:30]
     print(len(articles), 'not visited articles found in database')
     context = {
         'counts': get_db_counts(),
-        'articles': articles,
+        'articles': articles_top,
         'theme': 'dark',
     }
     return render_template('index.html', **context)
@@ -130,7 +65,7 @@ def notvisited():
 @app.route('/favorites', methods=["GET"])
 def favorites():
     articles = Url.query.filter_by(favorite=True).all()
-    articles = articles[::-1] # reverse order
+    articles = articles[::-1] # reverse order for latest first
     print(len(articles), 'favorite articles found in database')
     context = {
         'counts': get_db_counts(),
@@ -144,30 +79,35 @@ def favorites():
 @app.route('/visit', methods=["GET"])
 def visit():
     referer = request.headers.get("Referer")
-    _id = request.args.get("id")
+    story_id = request.args.get("id")
 
-    article = Url.query.filter_by(article_id=_id).first()
-
-    if article is not None:
-        if not article.visited:
-            article.visited = True
-            article.visit_date = getCurrentTime()
-            db.session.commit()
-            print(article.article_id, 'marked as visited')
-            return redirect(article.url)
-        else: print(article.article_id, 'already visited')
-    else: print('article not found in database')
+    if story_id is not None:
+        article = get_story_from_db(story_id)
+        if article is not None:
+            if not article.visited:
+                article.visited = True
+                article.visit_date = getCurrentTime()
+                db.session.commit()
+                print(article.article_id, 'marked as visited')
+                return redirect(article.url)
+            else: print(article.article_id, 'already visited')
+        else: print('article not found in database')
+    else: print('story_id not found')
 
     return redirect(referer or url_for('index'))
 
 @app.route('/favorite', methods=["GET"])
 def favorite():
     referer = request.headers.get("Referer")
-    _id = request.args.get("id")
-    article = Url.query.filter_by(article_id=_id).first()
-    if article is not None:
-        article.favorite = not article.favorite
-        db.session.commit()
-        print(article.article_id, f'favorite: {article.favorite}')
-    else: print('article not found in database')
+    story_id = request.args.get("id")
+
+    if story_id is not None:
+        article = get_story_from_db(story_id)
+        if article is not None:
+            article.favorite = not article.favorite
+            db.session.commit()
+            print(article.article_id, f'favorite: {article.favorite}')
+        else: print('article not found in database')
+    else: print('story_id not found')
+
     return redirect(referer or url_for('index'))
