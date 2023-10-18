acc6093cecf9d23043c60f588a2c44d4af56d557
fastfingertips <fastfingertips@gmail.com>
2023-10-18 23:44:05 +0300

    Import and use new date intervals library

--- routes.py
+++ routes.py
@@ -13,6 +13,7 @@ from functions import (
     getArticle,
     articleParser,
     getCurrentTime,
+    compareDates,
     get_story_from_db,
     get_stories
 )
@@ -65,23 +66,49 @@ def stats():
     all_articles = Url.query.filter_by(visited=True).all()
 
     today_visited_scores = []
+    yesterday_visited_scores = []
+    week_visited_scores = []
+    month_visited_scores = []
+    year_visited_scores = []
 
     for article in all_articles:
         if article.visit_date is not None:
-            if article.visit_date.date() == getCurrentTime().date():
+            article_date = article.visit_date.date()
+            current_time = getCurrentTime().date()
+            if compareDates(article_date, current_time):
                 today_visited_scores.append(article.score)
-
-    today_total_visited = len(today_visited_scores)
-    today_total_points = sum(today_visited_scores)
+            if compareDates(article_date, current_time, 1):
+                yesterday_visited_scores.append(article.score)
+            if compareDates(article_date, current_time, 7, False):
+                week_visited_scores.append(article.score)
+            if compareDates(article_date, current_time, 30, False):
+                month_visited_scores.append(article.score)
+            if compareDates(article_date, current_time, 365, False):
+                year_visited_scores.append(article.score)
 
     context = {
         'counts': get_db_counts(),
-        'today_total_visited': today_total_visited,
-        'today_total_points': today_total_points,
+
+        'today_total_visited': len(today_visited_scores),
+        'today_total_points': sum(today_visited_scores),
+        'yesterday_total_visited': len(yesterday_visited_scores),
+        'yesterday_total_points': sum(yesterday_visited_scores),
+        'week_total_visited': len(week_visited_scores),
+        'week_total_points': sum(week_visited_scores),
+        'month_total_visited': len(month_visited_scores),
+        'month_total_points': sum(month_visited_scores),
+        'year_total_visited': len(year_visited_scores),
+        'year_total_points': sum(year_visited_scores),
+
         'theme': 'dark',
     }
 
     return render_template('stats.html', **context)
+
+
+
+
+
 
 @app.route('/favorites', methods=["GET"])
 def favorites():
@@ -132,3 +159,26 @@ def favorite():
     else: print('story_id not found')
 
     return redirect(referer or url_for('index'))
+
+@app.route('/check_multiple', methods=["GET"])
+def check_multiple():
+    articles = Url.query.all()
+
+    for article in articles:
+        if article.article_id is not None:
+            articles = Url.query.filter_by(
+                article_id = article.article_id
+                ).all()
+            if len(articles) > 1:
+                print('multiple articles found in database')
+                for article in articles:
+                    if article.visited:
+                        print(article.article_id, 'multiple visited')
+                        for article in articles:
+                            if not article.visited:
+                                print(article.article_id, 'deleted from database')
+                                db.session.delete(article)
+                                db.session.commit()
+                        break
+
+    return redirect(url_for('index'))
