f518131982ee7c4a1b1068d24010008bb959406e
fastfingertips <fastfingertips@gmail.com>
2023-10-20 23:53:06 +0300

    Refactored configuration setup

--- main.py
+++ main.py
@@ -8,9 +8,12 @@ from routes import (
   favorites
 )
 
-# print(app.url_map)
+from config import Config
+
+# Configure the application using the "Config" class
+app.config.from_object(Config)
 
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    app.run()
