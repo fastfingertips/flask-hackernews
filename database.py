30d9976c1d2ce168f24ecfdde0df14ccfe858dca
fastfingertips <fastfingertips@gmail.com>
2023-10-20 23:25:36 +0300

    Update: database path

--- database.py
+++ database.py
@@ -2,10 +2,15 @@ from flask_sqlalchemy import SQLAlchemy
 from application import app
 import os
 
-app_path = os.path.dirname(os.path.abspath(__file__))
+# Define the SQLite database path
 db_name = "articles.db"
-db_path = os.path.join(app_path, db_name)
-sqlite_path = 'sqlite:///' + db_path
-app.config['SQLALCHEMY_DATABASE_URI'] = sqlite_path
 
+# Use the 'app.root_path' to get the root directory of your Flask application
+db_path = os.path.join(app.root_path, db_name)
+
+# Configure the SQLAlchemy database URI
+sqlite_uri = 'sqlite:///' + db_path
+app.config['SQLALCHEMY_DATABASE_URI'] = sqlite_uri
+
+# Create a SQLAlchemy database instance
 db = SQLAlchemy(app)
